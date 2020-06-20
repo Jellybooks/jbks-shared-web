ab4a60b245359a4fa670c7d0a1582fe4a9599ea3
JayPanoz <panozpublishingteam@gmail.com>
2020-06-20 17:50:51 +0200

    Add leadingPage() to ReadingProgression

--- r2-shared/Publication/ReadingProgression.ts
+++ r2-shared/Publication/ReadingProgression.ts
@@ -1,7 +1,25 @@
+import { Page } from "./presentation/Presentation";
+
 export enum ReadingProgression { 
   auto = "auto", 
   btt = "btt", 
   ltr = "ltr", 
   rtl = "rtl", 
   ttb = "ttb"
+}
+
+export namespace ReadingProgression {
+  export function leadingPage(readingProgression: ReadingProgression): Page {
+    switch(readingProgression) {
+      case ReadingProgression.auto:
+      case ReadingProgression.ttb:
+      case ReadingProgression.ltr:
+        return "left";
+      case ReadingProgression.rtl:
+      case ReadingProgression.btt:
+        return "right";
+      default:
+        return "left";
+    }
+  }
 }
